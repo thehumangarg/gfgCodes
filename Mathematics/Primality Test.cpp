1889cc0cd946bcd68d39216acde15543855a934f
Manav Garg <85007238+thehumangarg@users.noreply.github.com>
2021-10-28 22:07:21 +0530

    Create Primality Test.cpp

---
+++ Mathematics/Primality Test.cpp
@@ -0,0 +1,12 @@
+bool isPrime(int N)
+    {
+         if (N <= 1)
+        return false;
+        
+    for (int i = 2; i <= sqrt(N); i++)
+        if (N % i == 0)
+            return false;
+ 
+    return true;
+        
+    }
