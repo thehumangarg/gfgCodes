9256d517f294d58925d13dc58fc3a47d693c58ad
Manav Garg <85007238+thehumangarg@users.noreply.github.com>
2021-10-28 22:09:21 +0530

    Create Exactly 3 Divisors.cpp

---
+++ Mathematics/Exactly 3 Divisors.cpp
@@ -0,0 +1,42 @@
+class Solution{
+    public:
+    int exactly3Divisors(int N)
+    {
+       return printPrime(N);
+       
+    }
+    
+    bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+ 
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+ 
+    return true;
+}
+ 
+// Function to print primes
+int printPrime(int n)
+{
+    int count=0;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i))
+            if(i*i<=n)
+                count++;
+            else 
+                break;
+    }
+    return count;
+}
+};
